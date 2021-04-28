92b9b9287e14cb68c468e377ac000d3a1d1094b8
JaviDiesel <fjramos13@hotmail.com>
2021-04-28 13:11:53 +0200

    run2

--- Mobile.ts
+++ Mobile.ts
@@ -1,13 +1,13 @@
 export class Mobile {
 
-    public name:string;
-    public model:string;
-    public trademark:string;
-    public sdSize:number;
-    public color:string;
-    public is5G:boolean;
-    public cameraNumber:number;
-    public price:number;
+    private name:string;
+    private model:string;
+    private trademark:string;
+    private sdSize:number;
+    private color:string;
+    private is5G:boolean;
+    private cameraNumber:number;
+    private price:number;
 
     constructor(name:string, model:string, trademark:string, sdSize:number, color:string, is5G:boolean, cameraNumber:number, price:number)  {
         this.name = name;
@@ -18,5 +18,67 @@ export class Mobile {
         this.is5G = is5G;
         this.cameraNumber = cameraNumber;
         this.price = price;
+    }
+
+    public getName():string {
+        return this.name;
+    }
+    public getModel():string    {
+        return this.model;
+    }
+    public getTrademark():string   {
+        return this.trademark;
+    }
+    public getSdSize():number   {
+        return this.sdSize;
+    }
+    public getColor():string    {
+        return this.color;
+    }
+    public getIs5G():boolean {
+        return this.is5G;
+    }
+    public getCameraNumber():number    {
+        return this.cameraNumber;
+    }
+    public getPrice():number    {
+        return this.price;
+    }
+
+    public setName(name):void   {
+        this.name = name;
+    }
+    public setModel(model):void   {
+        this.model = model;
+    }
+    public setTrademark(trademark):void   {
+        this.trademark = trademark;
+    }
+    public setSdSize(sdSize):void   {
+        this.sdSize = sdSize;
+    }
+    public setColor(color):void   {
+        this.color = color;
+    }
+    public setIs5G(is5G):void   {
+        this.is5G = is5G;
+    }
+    public setCameraNumber(cameraNumber):void   {
+        this.cameraNumber = cameraNumber;
+    }
+    public setPrice(price):void {
+        this.price = price;
+    }
+
+    public printMobile():void   {
+        console.log(`The characteristics of the mobile ${this.name} are:`);
+        console.log(`Name: ${this.name}`);
+        console.log(`Model: ${this.model}`);
+        console.log(`Trademark: ${this.trademark}`);
+        console.log(`SD Size: ${this.sdSize}`);
+        console.log(`Color: ${this.color}`);
+        console.log(`Is 5g?: ${this.is5G}`);
+        console.log(`Number of Cameras: ${this.cameraNumber}`);
+        console.log('');
     }
 }
